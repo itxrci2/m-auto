bfacb806fb866871cd02aaf9111fddad7fd5e1c0
Tahseen <75565916+iTahseen@users.noreply.github.com>
2025-06-08 18:04:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from blocklist import (
     is_blocklist_active, add_to_blocklist, get_user_blocklist
 )
 
-API_TOKEN = "7610586859:AAFsDXtdd_rksn9QLLHm3n4GeMqsTgFDBKU"
+API_TOKEN = "7735279075:AAHvefFBqiRUE4NumS0JlwTAiSMzfrgTmqA"
 ADMIN_USER_IDS = [6387028671, 6816341239, 6204011131]
 TEMP_PASSWORD = "11223344"
 password_access = {}
